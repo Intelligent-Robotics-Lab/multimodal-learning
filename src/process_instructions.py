b3bd59706d746c8b23ebe48d9d927d329bc4e6a6
Alex Tyshka <atyshka@oakland.edu>
2022-11-15 16:31:45 -0500

    Added dependencies and installer

--- src/process_instructions.py
+++ src/process_instructions.py
@@ -1,7 +1,5 @@
 import json
-import spacy
 
-nlp = spacy.load("en_core_web_sm")
 with open('../data/wikihow-instructions.json') as f:
     wikihow_data = json.load(f).values()
 with open('../data/actions.txt', 'w+') as f:
@@ -15,9 +13,3 @@ with open('../data/actions.txt', 'w+') as f:
             continue
         else:
             f.write(instruction + '\n')
-        
-        doc = nlp("You should " + instruction)
-        if doc[2].pos_ != 'VERB':
-            print(doc)
-        # print(instruction)
-        # print(nlp(instruction)[2].pos_)
