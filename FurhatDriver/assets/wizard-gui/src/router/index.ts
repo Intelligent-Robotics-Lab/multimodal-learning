12fd0b0f99cf6e4a846fba805fb4aa8876d7c2e4
Alex Tyshka <atyshka15@gmail.com>
2023-02-09 11:46:17 -0500

    Latest GUI improvements

---
+++ FurhatDriver/assets/wizard-gui/src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import OperatorPanel from '../views/OperatorPanel.vue'
+
+const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: OperatorPanel
+    },
+    {
+      path: '/user',
+      name: 'user',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/UserPanel.vue')
+    }
+  ]
+})
+
+export default router
--- FurhatDriver/src/main/kotlin/furhatos/app/furhatdriver/flow/init.kt
+++ FurhatDriver/src/main/kotlin/furhatos/app/furhatdriver/flow/init.kt
@@ -4,14 +4,15 @@ import furhatos.app.furhatdriver.flow.main.Driver
 import furhatos.app.furhatdriver.setting.distanceToEngage
 import furhatos.app.furhatdriver.setting.maxNumberOfUsers
 import furhatos.flow.kotlin.*
+import furhatos.flow.kotlin.voice.AzureVoice
 import furhatos.flow.kotlin.voice.PollyNeuralVoice
 
 val Init : State = state() {
     init {
         /** Set our default interaction parameters */
         users.setSimpleEngagementPolicy(distanceToEngage, maxNumberOfUsers)
-        furhat.voice = PollyNeuralVoice.Joanna().also { it.style = PollyNeuralVoice.Style.Conversational}
-
+//        furhat.voice = PollyNeuralVoice.Joanna().also { it.style = PollyNeuralVoice.Style.Conversational}
+        furhat.voice = AzureVoice("Aria")
         /** start the interaction */
         goto(Driver)
     }
